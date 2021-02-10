ebb6c684dc866eac5743658638be8929571e6c3e
Jerron <jerron.mcginnis@gmail.com>
2021-02-10 16:19:13 -0500

    Established db connection

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+var http = require('http');
+var enforce = require('express-sslify');
+
+const PORT = process.env.PORT || 7000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.use(require("./routes/api.js"));
+ 
+http.createServer(app).listen(PORT, function() {
+    console.log('Express server listening on port ' + PORT);
+});
